930ae746203ea8e78253bde24a318f89576883e5
Camille Haus <camille_haus@hotmail.com>
2024-07-14 11:12:29 -0300

    Created new routes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify()
 
@@ -17,6 +21,10 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 app.listen({ port: 3333 }, () => {
     console.log("Server running!")
